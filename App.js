210f7f6d7e3c76aa4b0c67b7588587cb7393b886
Rajput <nitin98811234@gmail.com>
2024-01-31 23:17:00 +0530

    lecture 2 complete

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 // const heading = React.createElement(
 //   "h1",
 //   { id: "header", xyz: "nothing" },
@@ -17,8 +20,6 @@ const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("h2", {}, "I'm a h2 tag"),
   ]),
 ]);
-
-console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
