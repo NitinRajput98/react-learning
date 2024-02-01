934cdada0bb59f329e55cdab048b8bfff51dcdc3
Rajput <nitin98811234@gmail.com>
2024-02-02 01:13:59 +0530

    lecture 3 completed

--- App.js
+++ App.js
@@ -1,2 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+// React Element
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    This is a heading created using JSX.
+  </h1>
+);
+
+// Functional Component
+const HeadingComponent = () => {
+  return (
+    <div id="container">
+      {Title()}
+      <Title />
+      <Title></Title>
+      <h1 className="heading">This is React Functional Component.</h1>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent />);
