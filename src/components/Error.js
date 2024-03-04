a8f69e1fe13d9b5983eaccb04308c3c0b7438754
Rajput <nitin98811234@gmail.com>
2024-03-04 17:13:15 +0530

    routing added

---
+++ src/components/Error.js
@@ -0,0 +1,14 @@
+import { useRouteError } from "react-router-dom";
+
+const Error = () => {
+  const err = useRouteError();
+  console.log(err);
+  return (
+    <div>
+      <h1>{err.error.message}</h1>
+      <div>This is cutom error Page</div>
+    </div>
+  );
+};
+
+export default Error;
